233c6bd8331991620939fdcab89ef686bd77437c
Yashsangwan2002 <yashsangwan02.in@gmai.com>
2024-04-04 18:21:17 +0530

    upload button

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,29 @@
+const UploadButton = ({
+    isSubscribed,
+  }: {
+    isSubscribed: boolean
+  }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+  
+    return (
+      <Dialog
+        open={isOpen}
+        onOpenChange={(v) => {
+          if (!v) {
+            setIsOpen(v)
+          }
+        }}>
+        <DialogTrigger
+          onClick={() => setIsOpen(true)}
+          asChild>
+          <Button>Upload PDF</Button>
+        </DialogTrigger>
+  
+        <DialogContent>
+          <UploadDropzone isSubscribed={isSubscribed} />
+        </DialogContent>
+      </Dialog>
+    )
+  }
+  
+  export default UploadButton
