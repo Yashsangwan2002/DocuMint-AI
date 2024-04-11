32fadf74e3a5e7e2ef644ebd41eb265a85b0d8a1
Yashsangwan2002 <yashsangwan02.in@gmai.com>
2024-04-12 00:58:23 +0530

    error

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -8,7 +8,7 @@ import {
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { getPineconeClient } from '@/lib/pinecone'
+import { getPineconeClient } from '@lib/pinecone'
 import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { PLANS } from '@/config/stripe'
 
